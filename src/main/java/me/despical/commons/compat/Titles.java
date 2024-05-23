e9c9543d321d9ace8941be344e5fc6e98f2bfb80
Berke Akçen <berkeakcen@gmail.com>
2024-05-23 10:35:35 +0300

    Updated Titles

--- src/main/java/me/despical/commons/compat/Titles.java
+++ src/main/java/me/despical/commons/compat/Titles.java
@@ -18,41 +18,57 @@
 
 package me.despical.commons.compat;
 
-import me.despical.commons.ReflectionUtils;
-import org.bukkit.configuration.ConfigurationSection;
+import me.despical.commons.reflection.XReflection;
+import me.despical.commons.reflection.minecraft.MinecraftClassHandle;
+import me.despical.commons.reflection.minecraft.MinecraftPackage;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Objects;
-import java.util.function.Function;
+
+import static me.despical.commons.reflection.minecraft.MinecraftConnection.sendPacket;
+import static me.despical.commons.reflection.XReflection.ofMinecraft;
 
 /**
  * A reflection API for titles in Minecraft.
- * Fully optimized - Supports 1.8.8 and above.
+ * Fully optimized - Supports 1.8.8+ and above.
  * Requires ReflectionUtils.
  * Messages are not colorized by default.
  * <p>
  * Titles are text messages that appear in the
- * middle of the players screen: https://minecraft.gamepedia.com/Commands/title
- * PacketPlayOutTitle: https://wiki.vg/Protocol#Title
+ * middle of the players screen: <a href="https://minecraft.wiki/w/Commands/title">...</a>
+ * PacketPlayOutTitle: <a href="https://wiki.vg/Protocol#Title">...</a>
  *
+ * @author Crypto Morin
  * @author Despical
- * @version 1.2.1
- * @see <a href="https://github.com/CryptoMorin/XSeries">Original Code</a>
- * @see ReflectionUtils
+ * @version 3.1.0
+ * @see XReflection
  */
 public final class Titles implements Cloneable {
-
+	/**
+	 * EnumTitleAction
+	 * Used for the fade in, stay and fade out feature of titles.
+	 * Others: ACTIONBAR, RESET
+	 */
 	private static final Object TITLE, SUBTITLE, TIMES, CLEAR;
 	private static final MethodHandle PACKET_PLAY_OUT_TITLE;
+	/**
+	 * ChatComponentText JSON message builder.
+	 */
 	private static final MethodHandle CHAT_COMPONENT_TEXT;
 
 	private String title, subtitle;
 	private final int fadeIn, stay, fadeOut;
+
+	/**
+	 * From the latest 1.11.2 not checked with supports() to prevent
+	 * errors on outdated 1.11 versions.
+	 */
+	private static final boolean SUPPORTS_TITLES;
 
 	static {
 		MethodHandle packetCtor = null;
@@ -63,10 +79,24 @@ public final class Titles implements Cloneable {
 		Object subtitle = null;
 		Object clear = null;
 
-		if (!ReflectionUtils.supports(11)) {
-			Class<?> chatComponentText = ReflectionUtils.getNMSClass("ChatComponentText");
-			Class<?> packet = ReflectionUtils.getNMSClass("PacketPlayOutTitle");
-			Class<?> titleTypes = packet.getDeclaredClasses()[0];
+		boolean SUPPORTS_TITLES1;
+
+		try {
+			Player.class.getDeclaredMethod("sendTitle",
+				String.class, String.class,
+				int.class, int.class, int.class);
+			SUPPORTS_TITLES1 = true;
+		} catch (NoSuchMethodException e) {
+			SUPPORTS_TITLES1 = false;
+		}
+
+		SUPPORTS_TITLES = SUPPORTS_TITLES1;
+
+		if (!SUPPORTS_TITLES) {
+			MinecraftClassHandle chatComponentText = ofMinecraft().inPackage(MinecraftPackage.NMS).named("ChatComponentText");
+			MinecraftClassHandle packet = ofMinecraft().inPackage(MinecraftPackage.NMS).named("PacketPlayOutTitle");
+			MinecraftClassHandle IChatBaseComponentClass = ofMinecraft().inPackage(MinecraftPackage.NMS).named("IChatBaseComponent");
+			Class<?> titleTypes = packet.unreflect().getDeclaredClasses()[0];
 
 			for (Object type : titleTypes.getEnumConstants()) {
 				switch (type.toString()) {
@@ -84,14 +114,10 @@ public final class Titles implements Cloneable {
 				}
 			}
 
-			MethodHandles.Lookup lookup = MethodHandles.lookup();
 			try {
-				chatComp = lookup.findConstructor(chatComponentText, MethodType.methodType(void.class, String.class));
-
-				packetCtor = lookup.findConstructor(packet,
-					MethodType.methodType(void.class, titleTypes,
-						ReflectionUtils.getNMSClass("IChatBaseComponent"), int.class, int.class, int.class));
-			} catch (NoSuchMethodException | IllegalAccessException e) {
+				chatComp = chatComponentText.constructor(String.class).reflect();
+				packetCtor = packet.constructor(titleTypes, IChatBaseComponentClass.unreflect(), int.class, int.class, int.class).reflect();
+			} catch (ReflectiveOperationException e) {
 				e.printStackTrace();
 			}
 		}
@@ -132,30 +158,29 @@ public final class Titles implements Cloneable {
 	 * @param fadeOut  the amount of ticks for the title to fade out.
 	 * @param title    the title message.
 	 * @param subtitle the subtitle message.
-	 *
 	 * @see #clearTitle(Player)
 	 */
-	public static void sendTitle(Player player,
+	public static void sendTitle(@Nonnull Player player,
 								 int fadeIn, int stay, int fadeOut,
-								 String title, String subtitle) {
+								 @Nullable String title, @Nullable String subtitle) {
 		Objects.requireNonNull(player, "Cannot send title to null player");
 		if (title == null && subtitle == null) return;
-		if (ReflectionUtils.supports(11)) {
+		if (SUPPORTS_TITLES) {
 			player.sendTitle(title, subtitle, fadeIn, stay, fadeOut);
 			return;
 		}
 
 		try {
 			Object timesPacket = PACKET_PLAY_OUT_TITLE.invoke(TIMES, CHAT_COMPONENT_TEXT.invoke(title), fadeIn, stay, fadeOut);
-			ReflectionUtils.sendPacket(player, timesPacket);
+			sendPacket(player, timesPacket);
 
 			if (title != null) {
 				Object titlePacket = PACKET_PLAY_OUT_TITLE.invoke(TITLE, CHAT_COMPONENT_TEXT.invoke(title), fadeIn, stay, fadeOut);
-				ReflectionUtils.sendPacket(player, titlePacket);
+				sendPacket(player, titlePacket);
 			}
 			if (subtitle != null) {
 				Object subtitlePacket = PACKET_PLAY_OUT_TITLE.invoke(SUBTITLE, CHAT_COMPONENT_TEXT.invoke(subtitle), fadeIn, stay, fadeOut);
-				ReflectionUtils.sendPacket(player, subtitlePacket);
+				sendPacket(player, subtitlePacket);
 			}
 		} catch (Throwable throwable) {
 			throwable.printStackTrace();
@@ -169,49 +194,10 @@ public final class Titles implements Cloneable {
 	 * @param player   the player to send the title to.
 	 * @param title    the title message.
 	 * @param subtitle the subtitle message.
-	 *
 	 * @see #sendTitle(Player, int, int, int, String, String)
 	 */
-	public static void sendTitle(Player player, String title, String subtitle) {
+	public static void sendTitle(@Nonnull Player player, @Nonnull String title, @Nonnull String subtitle) {
 		sendTitle(player, 10, 20, 10, title, subtitle);
-	}
-
-	public static Titles parseTitle(ConfigurationSection config) {
-		return parseTitle(config, null);
-	}
-
-	/**
-	 * Parses a title from config.
-	 * The configuration section must at least
-	 * contain {@code title} or {@code subtitle}
-	 *
-	 * <p>
-	 * <b>Example:</b>
-	 * <blockquote><pre>
-	 *     ConfigurationSection titleSection = plugin.getConfig().getConfigurationSection("restart-title");
-	 *     Titles.sendTitle(player, titleSection);
-	 * </pre></blockquote>
-	 *
-	 * @param config the configuration section to parse the title properties from.
-	 */
-	public static Titles parseTitle(ConfigurationSection config, Function<String, String> transformers) {
-		String title = config.getString("title");
-		String subtitle = config.getString("subtitle");
-
-		if (transformers != null) {
-			title = transformers.apply(title);
-			subtitle = transformers.apply(subtitle);
-		}
-
-		int fadeIn = config.getInt("fade-in");
-		int stay = config.getInt("stay");
-		int fadeOut = config.getInt("fade-out");
-
-		if (fadeIn < 1) fadeIn = 10;
-		if (stay < 1) stay = 20;
-		if (fadeOut < 1) fadeOut = 10;
-
-		return new Titles(title, subtitle, fadeIn, stay, fadeOut);
 	}
 
 	public String getTitle() {
@@ -231,25 +217,13 @@ public final class Titles implements Cloneable {
 	}
 
 	/**
-	 * Parses and sends a title from the config.
-	 *
-	 * @param player the player to send the title to.
-	 * @param config the configuration section to parse the title properties from.
-	 */
-	public static Titles sendTitle(Player player, ConfigurationSection config) {
-		Titles titles = parseTitle(config, null);
-		titles.send(player);
-		return titles;
-	}
-
-	/**
 	 * Clears the title and subtitle message from the player's screen.
 	 *
 	 * @param player the player to clear the title from.
 	 */
-	public static void clearTitle(Player player) {
+	public static void clearTitle(@Nonnull Player player) {
 		Objects.requireNonNull(player, "Cannot clear title from null player");
-		if (ReflectionUtils.supports(11)) {
+		if (XReflection.supports(11)) {
 			player.resetTitle();
 			return;
 		}
@@ -262,7 +236,7 @@ public final class Titles implements Cloneable {
 			return;
 		}
 
-		ReflectionUtils.sendPacket(player, clearPacket);
+		sendPacket(player, clearPacket);
 	}
 
 	/**
@@ -277,20 +251,20 @@ public final class Titles implements Cloneable {
 	 * @param footer  the footer of the tablist.
 	 * @param players players to send this change to.
 	 */
-	public static void sendTabList(String header, String footer, Player... players) {
+	public static void sendTabList(@Nonnull String header, @Nonnull String footer, Player... players) {
 		Objects.requireNonNull(players, "Cannot send tab title to null players");
 		Objects.requireNonNull(header, "Tab title header cannot be null");
 		Objects.requireNonNull(footer, "Tab title footer cannot be null");
 
-		if (ReflectionUtils.supports(13)) {
+		if (XReflection.supports(13)) {
 			// https://hub.spigotmc.org/stash/projects/SPIGOT/repos/bukkit/browse/src/main/java/org/bukkit/entity/Player.java?until=2975358a021fe25d52a8103f7d7aaeceb3abf245&untilPath=src%2Fmain%2Fjava%2Forg%2Fbukkit%2Fentity%2FPlayer.java
 			for (Player player : players) player.setPlayerListHeaderFooter(header, footer);
 			return;
 		}
 
 		try {
-			Class<?> IChatBaseComponent = ReflectionUtils.getNMSClass("network.chat", "IChatBaseComponent");
-			Class<?> PacketPlayOutPlayerListHeaderFooter = ReflectionUtils.getNMSClass("network.protocol.game", "PacketPlayOutPlayerListHeaderFooter");
+			Class<?> IChatBaseComponent = XReflection.getNMSClass("network.chat", "IChatBaseComponent");
+			Class<?> PacketPlayOutPlayerListHeaderFooter = XReflection.getNMSClass("network.protocol.game", "PacketPlayOutPlayerListHeaderFooter");
 
 			Method chatComponentBuilderMethod = IChatBaseComponent.getDeclaredClasses()[0].getMethod("a", String.class);
 			Object tabHeader = chatComponentBuilderMethod.invoke(null, "{\"text\":\"" + header + "\"}");
@@ -306,7 +280,7 @@ public final class Titles implements Cloneable {
 			footerField.setAccessible(true);
 			footerField.set(packet, tabFooter);
 
-			for (Player player : players) ReflectionUtils.sendPacket(player, packet);
+			for (Player player : players) sendPacket(player, packet);
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
 		}
