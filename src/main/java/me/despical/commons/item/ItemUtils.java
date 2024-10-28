907636a407b41c85df79bbf67df3d7975bc4d006
Berke Akçen <berkeakcen@gmail.com>
2024-10-28 12:24:18 +0300

    Added skull texture support for 1.21.1+ Spigot servers

--- src/main/java/me/despical/commons/item/ItemUtils.java
+++ src/main/java/me/despical/commons/item/ItemUtils.java
@@ -20,6 +20,8 @@ package me.despical.commons.item;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.destroystokyo.paper.profile.ProfileProperty;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import me.despical.commons.compat.XMaterial;
@@ -31,6 +33,9 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.UUID;
 
 /**
@@ -66,23 +71,59 @@ public class ItemUtils {
 
 		if (XReflection.supports(21, 1)) {
 			UUID uuid = UUID.randomUUID();
-			PlayerProfile playerProfile = Bukkit.getServer().createProfile(UUID.randomUUID(), uuid.toString().substring(0, 16));
-			playerProfile.setProperty(new ProfileProperty("textures", url));
 
-			skullMeta.setPlayerProfile(playerProfile);
-		} else if (XReflection.supports(15)) {
+			try {
+				PlayerProfile playerProfile = Bukkit.getServer().createProfile(UUID.randomUUID(), uuid.toString().substring(0, 16));
+				playerProfile.setProperty(new ProfileProperty("textures", url));
+
+				skullMeta.setPlayerProfile(playerProfile);
+			} catch (Throwable ignored) {
+				try {
+					Method CREATE_PLAYER_PROFILE = Bukkit.getServer().getClass().getMethod("createPlayerProfile", String.class);
+					CREATE_PLAYER_PROFILE.setAccessible(true);
+					Object BUKKIT_PROFILE = CREATE_PLAYER_PROFILE.invoke(Bukkit.getServer(), uuid.toString().substring(0, 16));
+
+					Method GET_TEXTURES = BUKKIT_PROFILE.getClass().getMethod("getTextures");
+					GET_TEXTURES.setAccessible(true);
+					Object TEXTURES = GET_TEXTURES.invoke(BUKKIT_PROFILE);
+
+					String base64Value = new String(Base64.getDecoder().decode(url));
+					JsonObject jsonObject = new JsonParser().parse(base64Value).getAsJsonObject();
+					String newUrl = jsonObject
+						.getAsJsonObject("textures")
+						.getAsJsonObject("SKIN")
+						.get("url")
+						.getAsString();
+
+					Method SET_SKIN = TEXTURES.getClass().getMethod("setSkin", URL.class);
+					SET_SKIN.setAccessible(true);
+					SET_SKIN.invoke(TEXTURES, new URL("https://textures.minecraft.net/texture/" + newUrl));
+
+					Method SET_TEXTURES = Arrays.stream(BUKKIT_PROFILE.getClass().getMethods()).filter(method -> method.getName().equals("setTextures")).findFirst().get();
+					SET_TEXTURES.setAccessible(true);
+					SET_TEXTURES.invoke(BUKKIT_PROFILE, TEXTURES);
+
+					Method SET_OWNER_PROFILE = Arrays.stream(skullMeta.getClass().getMethods()).filter(method -> method.getName().equals("setOwnerProfile")).findFirst().get();
+					SET_OWNER_PROFILE.setAccessible(true);
+					SET_OWNER_PROFILE.invoke(skullMeta, BUKKIT_PROFILE);
+				} catch (Throwable ignored1) {
+				}
+			}
+		}
+
+		if (XReflection.supports(15)) {
 			try {
 				Method method = skullMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
 				method.setAccessible(true);
 				method.invoke(skullMeta, profile);
-			} catch (Exception ignored) {
+			} catch (Throwable ignored) {
 			}
 		} else {
 			try {
 				Field profileField = skullMeta.getClass().getDeclaredField("profile");
 				profileField.setAccessible(true);
 				profileField.set(skullMeta, profile);
-			} catch (Exception ignored) {
+			} catch (Throwable ignored) {
 			}
 		}
 
